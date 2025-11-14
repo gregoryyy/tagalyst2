387aab670d201c06a2421bf4c57cc24b1b01b8fc
Gregor Heinrich <gregor@arbylon.net>
2025-11-14 07:39:10 +0100

    Add object-oriented class structure based on options.ts

---
+++ src/optionsOO.ts
@@ -0,0 +1,25 @@
+class ConfigManager {
+    getConfig() {
+        // Implementation for getting config
+    }
+
+    saveConfig(config) {
+        // Implementation for saving config
+    }
+}
+
+class StorageManager {
+    getStorageUsage() {
+        // Implementation for getting storage usage
+    }
+}
+
+class UIManager {
+    init() {
+        // Implementation for initializing UI
+    }
+
+    // Additional DOM manipulation methods
+}
+
+// Additional helper classes can be included here as necessary.
